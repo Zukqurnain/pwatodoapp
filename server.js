a04fb9271c3e4ac01c95737e6ace9cd97f47058c
adeelhussain <mr.adi180@gmail.com>
2018-09-12 02:12:30 +0500

    add dynamic port

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var app = express();
 
 app.use(express.static(__dirname + '/www'));
 
-app.listen('3000');
-console.log('working on 3000');
+app.listen(process.env.PORT || 3000);
+console.log('working on', process.env.PORT);
